8155b4aaa78812043ada60ea995c4a697c92c89b
xialei <377284071@qq.com>
2018-11-25 22:39:35 +0800

    11.25

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const Koa = require("koa");
 const app = new Koa();
-
-app.use(async (ctx, next) => {
-	await next();
-	ctx.response.type = 'text/html';
-	ctx.response.body = '<h1>hello, koa2!</h1>'	
-})
+const controller = require('./controller');
+app.use(controller());
 
 app.listen(3000);
 console.log('app start at port 3000....')
